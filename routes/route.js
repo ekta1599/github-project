5aef99a76e98bc8c5de992602ba2501a807a2129
ekta1599 <ektaalagiya.itcodehelp@gmail.com>
2023-10-17 16:53:27 +0530

    crud operation

--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,9 @@
 const router = require('express')
 const route = router()
 const controller = require('../controller/controller')
+
 route.post('/add',controller.add)
+route.put("/update/:_id", controller.update)
+route.delete('/delete/:_id', controller.delete)
 
 module.exports = route
