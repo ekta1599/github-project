44eb90deafec6a28a67cb3721ff4946f5f439ae9
ekta1599 <ektaalagiya.itcodehelp@gmail.com>
2023-10-18 14:35:03 +0530

    cors handle

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req,res) => {
 })
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({origin: '*'}));
 app.use(morgan("tiny"));
 app.use('/',v1)
 
