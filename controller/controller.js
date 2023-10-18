d03234fdbcfcd5be328eb4938d982665fa5feca0
ekta1599 <ektaalagiya.itcodehelp@gmail.com>
2023-10-18 10:07:50 +0530

    getall and getbyId api

--- controller/controller.js
+++ controller/controller.js
@@ -39,3 +39,30 @@ exports.delete = async (req, res) => {
         return response(err.message, err?.error, err.status, res);
     }
 };
+exports.byId = async (req, res) => {
+    try {
+      let resp = await service.byId(req.params._id);
+      if (resp) {
+        return response("SUCCESS..!!", resp.data, 200, res);
+      } else {
+        return response("Error..!!", err.error, err.status, res);
+      }
+    } catch (err) {
+      return response(err.message, err?.error, err.status, res);
+    }
+  };
+  exports.getAll = async (req, res) => {
+    try {
+      if (!req.query.page || !req.query.limit) {
+        return response("pagination is require for pagination..!!", {}, 404, res);
+      } else {
+        let resp = await service.getAll(req.query.page,
+            req.query.limit,);
+        if (resp) {
+          return response("SUCCESS..!!", resp.data, 200, res);
+        } else return response("Error..!!", err.error, err.status, res);
+      }
+    } catch (err) {
+      return response(err.message, err?.error, err.status, res);
+    }
+  };
