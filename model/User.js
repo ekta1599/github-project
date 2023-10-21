24cedc9abb4891b18d970e5f02bb27fb080795a1
ekta1599 <ektaalagiya.itcodehelp@gmail.com>
2023-10-21 10:32:17 +0530

    login and registration api

---
+++ model/User.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose')
+
+let UserModel = new Schema(
+    {
+        first_name: {
+            type: String,
+            default: null,
+        },
+        last_name: {
+            type: String,
+            default: null
+        },
+        email: {
+            type: String,
+            unique: true
+        },
+        password: {
+            type: String,
+        },
+        token: {
+            type: String,
+
+        }
+
+    },
+    { timestamps: true }
+);
+
+module.exports = model("UserModel", UserModel);
--- routes/route.js
+++ routes/route.js
@@ -1,10 +1,90 @@
 const router = require('express')
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
 const route = router()
 const controller = require('../controller/controller')
-
+const {response} = require('../middleware/response')
+const UserModel = require('../model/User')
+const auth = require('../middleware/auth')
 route.post('/add',controller.add)
 route.put("/update/:_id", controller.update)
 route.delete('/delete/:_id', controller.delete)
 route.get('/getbyId/:_id',controller.byId)
 route.get('/getAll',controller.getAll)
+route.post("/register", async (req, res) => {
+    try {
+      const { first_name, last_name, email, password } = req.body;
+  
+      if (!(email && password && first_name && last_name)) {
+        res.status(400).send("All input is required");
+      }
+      const oldUser = await UserModel.findOne({ email });
+  
+      if (oldUser) {
+        return res.status(409).send("User Already Exist. Please Login");
+      }
+  
+      encryptedPassword = await bcrypt.hash(password, 10);
+  
+      const user = await UserModel.create({
+        first_name,
+        last_name,
+        email: email.toLowerCase(), 
+        password: encryptedPassword,
+      });
+  
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+      user.token = token;
+      if(user){
+        return response("Success!!", {}, 200, res);
+      }else{
+        return response("data not added!!", {}, 404, res);
+      }
+    } catch (err) {
+      console.log("err",err);
+      return response("something went wrong!!", {}, 500, res);
+    }
+
+  });
+route.post("/login", async (req, res) => {
+    try {
+     
+      const { email, password } = req.body;
+  
+      if (!(email && password)) {
+        res.status(400).send("All input is required");
+      }
+      
+      const user = await UserModel.findOne({ email });
+  
+      if (user && (await bcrypt.compare(password, user.password))) {
+       
+        const token = jwt.sign(
+          { user_id: user._id, email },
+          process.env.TOKEN_KEY,
+          {
+            expiresIn: "2h",
+          }
+        );
+  
+    
+        user.token = token;
+  
+        res.status(200).json(user);
+      }
+      res.status(400).send("Invalid Credentials");
+    } catch (err) {
+      console.log(err);
+    }
+  
+  });
+  route.post("/welcome", auth, (req, res) => {
+    res.status(200).send("Welcome 🙌 ");
+  });
 module.exports = route
